c1c140cc019268e32568365e03fa0912cc66bdd9
boegeskov <58292148+boegeskov@users.noreply.github.com>
2021-05-12 14:26:55 +0200

    Add files via upload

---
+++ routes/chat.js
@@ -0,0 +1,14 @@
+const router = require("express").Router(); // den sidste parantes instansere linjen. // dummy route
+
+
+
+
+router.get("/api/chat", (req, res) => {
+    
+});
+
+
+// a variable you assaign something too
+module.exports = {
+    router
+};
